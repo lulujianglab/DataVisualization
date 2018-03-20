ff91121d3ce7d2ca5ec15fd581c232b2ba475680
lulujianglab <18810330250@163.com>
2018-03-20 18:15:10 +0800

    bug fix

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -24,6 +24,9 @@ module.exports = {
   entry: {
     app: './src/main.js'
   },
+  externals: {
+    "echarts": "echarts"
+  },
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
